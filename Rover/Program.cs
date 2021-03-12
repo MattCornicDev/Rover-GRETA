73a450f54cb41c84f6a52a3889352d0ab9910010
MattCornicDev <glgharib@gmail.com>
2021-03-12 15:22:57 +0100

    Projet Rover C#

---
+++ Rover/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace Robot
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Byte[] Avant = new byte[] { 0x70, 0x70 };
+            Byte[] Arriere = new byte[] { 0x30, 0x30 };
+            TcpClient SocketClient = new TcpClient();
+            NetworkStream NetStream;
+            int i = 0;
+            try
+            {
+                SocketClient.Connect("localhost", 15020);
+                NetStream = SocketClient.GetStream();
+                NetStream.Write(Avant, 0, 2);
+                NetStream.Write(Arriere, 0, 2);
+                Thread.Sleep(2000);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Erreur à la connexion" + e);
+            }
+        }
+    }
+}
---
+++ Rover/Robot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace robot_IHM
+{
+
+
+    class Robot
+    {
+        private Byte[] Commande;
+        private TcpClient SocketClient;
+        private string Serveur;
+        private int Port;
+        private NetworkStream NetStream;
+        public Robot(string _Serveur, int _Port)
+        {
+            Port = _Port;
+            Serveur = _Serveur;
+            // Instanciation du serveur
+            SocketClient = new TcpClient();
+            // Connexion au serveur
+            try {SocketClient.Connect(Serveur, Port); }
+            catch(SocketException e)
+            {
+                Console.WriteLine("Erreur à la connexion" + e);
+            }
+            NetStream = SocketClient.GetStream();
+
+        }
+
+        public void Commander(Byte[] Ordre)
+        {
+            Commande = Ordre;
+            NetStream.Write(Commande, 0, 2);
+        }
+
+
+          
+
+    }
+}
